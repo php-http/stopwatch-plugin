eb2d254ad720779d9168e8921b1fa7d64c3c1ddf
David Buchmann <david.buchmann@liip.ch>
2022-01-14 17:06:39 +0100

    run phpstan

--- src/StopwatchPlugin.php
+++ src/StopwatchPlugin.php
@@ -4,6 +4,7 @@ namespace Http\Client\Common\Plugin;
 
 use Http\Client\Common\Plugin;
 use Http\Client\Exception;
+use Http\Promise\Promise;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
@@ -29,17 +30,20 @@ final class StopwatchPlugin implements Plugin
         $this->stopwatch = $stopwatch;
     }
 
+    /**
+     * @return Promise Resolves a PSR-7 Response or fails with an Http\Client\Exception (The same as HttpAsyncClient)
+     */
     protected function doHandleRequest(RequestInterface $request, callable $next, callable $first)
     {
         $eventName = $this->getStopwatchEventName($request);
         $this->stopwatch->start($eventName, self::CATEGORY);
 
         return $next($request)->then(function (ResponseInterface $response) use ($eventName) {
-            $this->stopwatch->stop($eventName, self::CATEGORY);
+            $this->stopwatch->stop($eventName);
 
             return $response;
         }, function (Exception $exception) use ($eventName) {
-            $this->stopwatch->stop($eventName, self::CATEGORY);
+            $this->stopwatch->stop($eventName);
 
             throw $exception;
         });
